0476eaf7c1ffe547bb7f633ef445ec34aa0067cf
Xiao <xksaturn@gmail.com>
2024-09-14 17:02:03 +0800

    Update _document.tsx
    Add Google Adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3587655547787611"
+     crossorigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
